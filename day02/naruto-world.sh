974383751be2ee7d585c4644364cbb45da765ed4
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-07-10 23:19:50 +0530

    added new code

---
+++ day02/naruto-world.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This is Naruto ki Duniya
+
+<< comment
+We can write anything in this block, compiler will not comsider it as a code
+comment
+
+name1="Naruto Uzumaki"
+life_goal="Hokage"
+echo "I am $name1, I want to become future $life_goal | $(date)"
+
+echo "What Motivate you:"
+read userinput
